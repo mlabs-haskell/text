056b2e110d246b9dff1cb55bc50501e2f058087e
Bodigrim <andrew.lelechenko@gmail.com>
2021-05-22 21:29:43 +0100

    Revert "Implement utf8LengthByLeader via bit magic", because it hurts take/drop performance
    This reverts commit 2256d55df542248c65e1540254cf117d7eb6625f.
    
    # Conflicts:
    #	src/Data/Text/Internal/Encoding/Utf8.hs

--- src/Data/Text/Internal/Encoding/Utf8.hs
+++ src/Data/Text/Internal/Encoding/Utf8.hs
@@ -75,19 +75,12 @@ utf8Length c = word64ToInt $ 4 - (magic `unsafeShiftR` wordToInt (bitLength `shi
     magic :: Word64
     magic = 0b0101010101101010101111111111111111
 
--- This is a branchless version of
--- utf8LengthByLeader w
---   | w < 0x80  = 1
---   | w < 0xE0  = 2
---   | w < 0xF0  = 3
---   | otherwise = 4
 utf8LengthByLeader :: Word8 -> Int
-utf8LengthByLeader w = wordToInt $ 4 - (magic `unsafeShiftR` wordToInt (bitLength `shiftL` 1) .&. 3)
-  where
-    bitLength :: Word
-    bitLength = intToWord (countLeadingZeros (maxBound - w))
-    magic :: Word
-    magic = 0b01101011
+utf8LengthByLeader w
+  | w < 0x80  = 1
+  | w < 0xE0  = 2
+  | w < 0xF0  = 3
+  | otherwise = 4
 
 ord2' :: Char -> (Int, Int)
 ord2' c = (x1, x2)
@@ -283,12 +276,6 @@ intToWord = fromIntegral
 
 word64ToInt :: Word64 -> Int
 word64ToInt = fromIntegral
-
--- word16ToInt :: Word16 -> Int
--- word16ToInt = fromIntegral
-
--- word8ToInt :: Word8 -> Int
--- word8ToInt = fromIntegral
 
 wordToInt :: Word -> Int
 wordToInt = fromIntegral
