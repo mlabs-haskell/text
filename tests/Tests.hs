1a400c582cb22ff7abdbfe0a458d500036ce9a7d
Bodigrim <andrew.lelechenko@gmail.com>
2021-05-25 20:21:03 +0100

    Revert "Disable Tests.Lift on Windows due to strange linker error"
    This reverts commit f03abdca705b1d18d666669466a472c17706e233.

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -6,25 +6,20 @@ module Main
 
 import Test.Tasty (defaultMain, testGroup)
 
+import qualified Tests.Lift as Lift
 import qualified Tests.Properties as Properties
 import qualified Tests.Regressions as Regressions
 
-#ifndef mingw32_HOST_OS
-import qualified Tests.Lift as Lift
-#endif
-
-#ifndef ASSERTS
+#if !defined(ASSERTS)
 import qualified Tests.Inspection.Strict as InspectionStrict
 import qualified Tests.Inspection.Lazy   as InspectionLazy
 #endif
 
 main :: IO ()
 main = defaultMain $ testGroup "All"
-  [ Properties.tests
+  [ Lift.tests
+  , Properties.tests
   , Regressions.tests
-#ifndef mingw32_HOST_OS
-  , Lift.tests
-#endif
 #if !defined(ASSERTS)
   , InspectionStrict.tests
   , InspectionLazy.tests
